5ce61cff7f7a63105596e498cd24e994ce1902b9
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2025-03-12 13:01:54 +0400

    Update MySearchController.cs

--- CS/EFCore/ComplexSearchEF/ComplexSearchEF.Module/Controllers/MySearchController.cs
+++ CS/EFCore/ComplexSearchEF/ComplexSearchEF.Module/Controllers/MySearchController.cs
@@ -5,26 +5,17 @@ using dxTestSolution.Module.BusinessObjects;
 using MySolution.Module.BusinessObjects;
 
 namespace dxTestSolution.Module.Controllers {
-
     public class MySearchController : ObjectViewController<DetailView, MySearchClass> {
         public MySearchController() {
             var myAction1 = new SimpleAction(this, "MySearch", "MySearchCategory");
             myAction1.Execute += MyAction1_Execute;
-
         }
-
         private void MyAction1_Execute(object sender, SimpleActionExecuteEventArgs e) {
             var mySearchObject = (MySearchClass)View.CurrentObject;
-
             var persistentOS = Application.CreateObjectSpace(typeof(Contact));
             var criterion = CriteriaOperator.FromLambda<Contact>(x => x.FirstName.Contains(mySearchObject.FirstName) || x.Age == mySearchObject.Age);
-
             var results = persistentOS.GetObjects<Contact>(criterion);
-
             mySearchObject.SetContacts(results);
-
         }
-
-
     }
 }
